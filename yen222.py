ab269d5d447f176da74dfa80807624e5a4b0f479
nana6692 <56088948+nana6692@users.noreply.github.com>
2021-03-03 06:19:59 +0900

    Update yen222.py

--- yen222.py
+++ yen222.py
@@ -1,20 +1,21 @@
 import discord
+import os
 
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print(client.user.id)
-    print("ready")
+    print("정상구동")
     game = discord.Game("인사")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
-
+@client.event
+async def on_message(message):
+    if message.content.startswith("인사"):
+        await message.channel.send("하이")
+        
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
-
-
-
-
-client.run("d7f0ea40723365a33d83867054803febbdf8c8d2c7450f60e93f2f6ed9289fce")
